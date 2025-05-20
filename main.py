53327fe7bb942f99fa3a81135cd286ba9b5e784a
Anthony DeMatteo <anthde13@gmail.com>
2025-05-20 12:01:41 -0400

    - file path info is now a raw string literal, so it doesn't treat \ like newline. - get parent folder name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
 
-file_path = input ("Enter the file path: ")
-print("File path: ", file_path)
+file_path = input (r"Enter the file path: ")
+
+parent_folder_name = os.path.basename(os.path.dirname(file_path))
+print(parent_folder_name)
