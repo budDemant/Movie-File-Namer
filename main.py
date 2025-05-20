e45e977b862bbc9c087e5f74dbab3996dc6fa9b9
Anthony DeMatteo <anthde13@gmail.com>
2025-05-20 12:29:23 -0400

    - listdir returns all files and directories of user-defined folder. - join combines the user-defined folder path with the file name. - isfile checks for files, not sub-directories. - getsize returns the size of each file (in bytes). - those file sizes are put in a list and returned.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
 
-file_path = input (r"Enter the file path: ")
+folder_path = input (r"Enter the folder path: ")
 
-parent_folder_name = os.path.basename(os.path.dirname(file_path))
-print(parent_folder_name)
+folder_name = os.path.basename(folder_path)
+
+# size = os.path.getsize(r"C:\Users\anthd\Desktop\test2\writing.txt")
+# print(size)
+
+file_size = []
+
+for file in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, file)
+    if os.path.isfile(file_path): # check if file, not folder
+        size = os.path.getsize(file_path)
+        file_size.append(size)
+
+print(file_size)
