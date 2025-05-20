93736fef9ec2b8a08e54199dc8a879a12addc1d5
Anthony DeMatteo <anthde13@gmail.com>
2025-05-20 13:54:40 -0400

    - find the largest file in a dictionary that contains file names with their corresponding file sizes - rename the largest file in that dictionary to the parent folder name - create a folder called "Extras" (if it doesn't exist already).

--- main.py
+++ main.py
@@ -4,15 +4,28 @@ folder_path = input (r"Enter the folder path: ")
 
 folder_name = os.path.basename(folder_path)
 
-# size = os.path.getsize(r"C:\Users\anthd\Desktop\test2\writing.txt")
-# print(size)
-
-file_size = []
+file_size = {}
 
 for file in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file)
     if os.path.isfile(file_path): # check if file, not folder
         size = os.path.getsize(file_path)
-        file_size.append(size)
+        file_size[file] = size
 
-print(file_size)
+largest_file = max(file_size.values())
+
+for key, value in file_size.items():
+    if value == largest_file:
+        old_path = os.path.join(folder_path, key)
+        name, ext = os.path.splitext(key)
+        new_path = os.path.join(folder_path, folder_name + ext)
+        os.rename(old_path, new_path)
+        print(key, 'renamed to', folder_name + ext)
+    else:
+        extras_path = os.path.join(folder_path, 'Extras')
+        # Create Extras folder if it doesn't already exist
+        os.makedirs(extras_path, exist_ok=True)
+        
+        # old_path = os.path.join(folder_path, key)
+        
+        
